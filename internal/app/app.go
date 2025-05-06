f018bc00041411cc641b2deb54b843518e88856d
Semkufu95 <semkufu95@gmail.com>
2025-05-07 02:47:14 +0300

    Updated workoutstore.go

--- internal/app/app.go
+++ internal/app/app.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/semkufu95/go_project/internal/api"
 	"github.com/semkufu95/go_project/internal/store"
+	"github.com/semkufu95/go_project/migrations"
 )
 
 type Application struct {
@@ -21,6 +22,11 @@ func NewApplication() (*Application, error) {
 	pgDB, err := store.Open()
 	if err != nil {
 		return nil, err
+	}
+
+	err = store.MigrateFS(pgDB, migrations.FS, ".")
+	if err != nil {
+		panic(err)
 	}
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
